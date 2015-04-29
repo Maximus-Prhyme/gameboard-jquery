b8e131045b72545a66216575674de48950209f56
Maximo Rosario <xam527@gmail.com>
2015-04-29 16:36:56 -0400

    Added two drop menus and notification bell

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var main = function() {
-
+$('.more-btn').click(function() {
+	// console.log('this'));
+	$(this).next().toggle();
+	// $('.more-menu').toggle();
+})
+$('.share').click(function(){
+	$(this).next().toggle();
+})	
+$('.notification').click(function(){
+	$(this).toggleClass('active');
+})
 };
 
 $(document).ready(main);
